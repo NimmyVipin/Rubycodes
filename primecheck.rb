2890ec97e310694b4c433c7a2bc2677195ab2881
NimmyVipin <nimmyvipin25@gmail.com>
2016-10-27 11:56:15 +0530

    Update primecheck.rb

--- primecheck.rb
+++ primecheck.rb
@@ -1,12 +1,12 @@
 require 'prime'
-puts "Enter a number"
 
+puts "Enter a number"
 num = gets.chomp.to_i
 for i in 2..num/2
-if num%i==0
-puts " #{num} is not prime"
-break
-end
+  if num%i==0
+    puts " #{num} is not prime"
+    break
+  end
 puts " #{num} is prime"
 end
 
